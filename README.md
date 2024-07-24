0203bf12aa2fa498b1a71523cb43c5e1462f6570
Ignacio Barros <barros_ignacio1714@yahoo.com>
2024-07-24 17:46:18 -0300

    Hash passwords and update data structure

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # auth-mg
+
+Password Client: Asd12345@_
+Password Professional: Asd12345@_
--- init-mongo.js
+++ init-mongo.js
@@ -4,8 +4,8 @@ db = db.getSiblingDB('testdb'); // Selecciona la base de datos, la creará si no
 var client = JSON.parse(cat('data/User/client.json'));
 var professional = JSON.parse(cat('data/User/professional.json'));
 
-// Inserta los datos en la colección llamada "users"
-db.users.insertMany(client);
-db.users.insertMany(professional);
+// Inserta los datos en la colección llamada "User"
+db.User.insertMany(client);
+db.User.insertMany(professional);
 
 print('Datos de prueba cargados');
