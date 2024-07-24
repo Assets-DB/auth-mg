77c61879de532eb6a1a9779ea553883f36d89e2a
Ignacio Barros <barros_ignacio1714@yahoo.com>
2024-07-24 18:34:03 -0300

    Fix errors in [init-mongo.js]

--- init-mongo.js
+++ init-mongo.js
@@ -4,8 +4,20 @@ db = db.getSiblingDB('testdb'); // Selecciona la base de datos, la creará si no
 var client = JSON.parse(cat('data/User/client.json'));
 var professional = JSON.parse(cat('data/User/professional.json'));
 
-// Inserta los datos en la colección llamada "User"
-db.User.insertMany(client);
-db.User.insertMany(professional);
+// Función para convertir _id de string a ObjectId
+function convertToObjectId(doc) {
+    if (doc._id && doc._id.$oid) {
+        doc._id = ObjectId(doc._id.$oid);
+    }
+    return doc;
+}
+
+// Convertir el _id de cada documento en el array
+client = client.map(convertToObjectId);
+professional = professional.map(convertToObjectId);
+
+// Inserta los datos en la colección llamada "user"
+db.user.insertMany(client);
+// db.user.insertMany(professional);
 
 print('Datos de prueba cargados');
