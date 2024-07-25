e726df41b95692a13bbdca37acf03d3436c38d0e
Ignacio Barros <barros_ignacio1714@yahoo.com>
2024-07-24 23:50:45 -0300

    Configure MongoDB replica set and update initialization script

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,14 @@
 # Usa la imagen oficial de MongoDB
 FROM mongo:5.0
 
-# Copia el archivo de datos de prueba al contenedor
-COPY ./data /data
+# Copia los archivos de datos de prueba al contenedor
+COPY ./data /docker-entrypoint-initdb.d/data
+
+# Copia el script de inicialización al directorio de inicialización de MongoDB
 COPY ./init-mongo.js /docker-entrypoint-initdb.d/
+
+# Configura MongoDB para usar un conjunto de réplicas
+CMD ["mongod", "--replSet", "rs0", "--bind_ip_all"]
 
 # Exponemos el puerto 27017
 EXPOSE 27017
--- init-mongo.js
+++ init-mongo.js
@@ -1,13 +1,26 @@
-db = db.getSiblingDB('testdb'); // Selecciona la base de datos, la creará si no existe
+rs.initiate();
 
-// Carga datos de prueba desde un archivo JSON
-var client = JSON.parse(cat('data/User/client.json'));
-var professional = JSON.parse(cat('data/User/professional.json'));
+// Esperar hasta que el conjunto de réplicas esté completamente inicializado
+while (!db.isMaster().ismaster) {
+    print("Esperando a que el conjunto de réplicas se inicialice...");
+    sleep(1000);
+}
+
+// Seleccionar la base de datos, la creará si no existe
+db = db.getSiblingDB('testdb'); 
+
+// Cargar datos de prueba desde un archivo JSON
+var client = JSON.parse(cat('/docker-entrypoint-initdb.d/data/User/client.json'));
+var professional = JSON.parse(cat('/docker-entrypoint-initdb.d/data/User/professional.json'));
 
 // Función para convertir _id de string a ObjectId
 function convertToObjectId(doc) {
     if (doc._id && doc._id.$oid) {
         doc._id = ObjectId(doc._id.$oid);
+        doc.last_login = new Date(doc.last_login.$date);
+        doc.created_at = new Date(doc.created_at.$date);
+        doc.updated_at = new Date(doc.updated_at.$date);
+        doc.deleted_at = new Date(doc.deleted_at.$date);
     }
     return doc;
 }
@@ -16,7 +29,7 @@ function convertToObjectId(doc) {
 client = client.map(convertToObjectId);
 professional = professional.map(convertToObjectId);
 
-// Inserta los datos en la colección llamada "user"
+// Insertar los datos en la colección llamada "user"
 db.user.insertMany(client);
 db.user.insertMany(professional);
 
